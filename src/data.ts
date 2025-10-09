20ff351c73edeb1e9fd99ef7bcbf6f46cd865089
UrielGGarcia <227o01221@itsx.edu.mx>
2025-10-09 15:25:39 -0600

    fix(home): arreglar la responsividad de componentes en la pagina home

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,9 @@
 // Datos para llenar y probar
 
 interface Book {
+    id: number,
     urlImg: string,
-    titulo: string
+    titulo: string,
 }
 
 interface Autor {
@@ -14,22 +15,22 @@ interface Genero {
 }
 
 export const Books: Book[] = [
-    { urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
-    { urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
-    { urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" }
+    { id: 1, urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 2,urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 3,urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 4,urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 5,urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 6,urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 7,urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 8,urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 9,urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 10,urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 11,urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 12,urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 13,urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 14,urlImg: "https://m.media-amazon.com/images/I/71oNYYiX3PL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" },
+    { id: 15,urlImg: "https://m.media-amazon.com/images/I/91u4JgsroxL._SL1500_.jpg", titulo: "Metro 2033 (NE)" },
+    { id: 16,urlImg: "https://m.media-amazon.com/images/I/91sTC4V37qL._SL1500_.jpg", titulo: "Guerra Mundial Z: Una Historia Oral de la Guerra Zombi" }
 ]
 
 
